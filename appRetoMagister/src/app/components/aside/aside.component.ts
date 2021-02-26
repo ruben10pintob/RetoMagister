c3ff06ba6cd09c3588e17fcad383aafd78922af1
ruben <ruben10pintob@gmail.com>
2021-02-26 16:04:59 +0100

    Código HTML del componte home y programacion básica

--- appRetoMagister/src/app/components/aside/aside.component.ts
+++ appRetoMagister/src/app/components/aside/aside.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-aside',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsideComponent implements OnInit {
 
-  constructor() { }
+  @Input() linkActive: string;
+
+  public opciones: Array<string>;
+  public icon_default: string;
+  public icon_active: string;
+
+  constructor() { 
+
+    this.opciones = [
+      '¿En qué te quieres especializar?',
+      'Horario y Modalidad',
+      'Tarifa',
+      'Datos personales',
+      'Dirección',
+      'Forma de pago'
+    ],
+
+    this.icon_default = "fas fa-circle",
+    this.icon_active = "far fa-circle"
+
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
