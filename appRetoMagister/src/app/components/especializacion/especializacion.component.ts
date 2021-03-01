a805c67564cac88e996388a773e882db565f0244
ruben <ruben10pintob@gmail.com>
2021-03-01 16:03:12 +0100

    Código HMTL con maqutación  básica y funcionalidad al boton siguiente

--- appRetoMagister/src/app/components/especializacion/especializacion.component.ts
+++ appRetoMagister/src/app/components/especializacion/especializacion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-especializacion',
@@ -11,11 +12,16 @@ export class EspecializacionComponent implements OnInit {
   public user: User;
 
   constructor(
+    private _router: Router,
   ) { 
     this.user = new User('','','','','','','','','','','','','','','','');
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this._router.navigate(['/horario-modalidad']);
   }
 
 }
